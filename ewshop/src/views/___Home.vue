4eb4f2cfc600a2d5c61e3a6db290b499211d094d
NingZ <ninng_0436@163.com>
2021-09-24 19:21:05 +0800

    master

---
+++ ewshop/src/views/___Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <ul>
+      <li v-for="item in banner" :key="item.id">
+        {{item.id}}--{{item.title}}<br>
+        <img :src="item.img_url" alt=""></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { onMounted, ref } from 'vue';
+import { getHomeAllData } from "../network/home.js";
+export default {
+  name: 'Home',
+  setup(){
+    const banner = ref([]);
+    onMounted(()=>{
+      
+      getHomeAllData().then(res=>{
+        banner.value = res.slides;
+        console.log(res.slides);
+      }).catch(err=>{
+
+      })
+    })
+    return {
+      banner
+    }
+  },
+  components:{
+
+  }
+}
+</script>
